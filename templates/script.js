7faa1dd0a09148d5115f657276f9cd67633eca7e
wookiee2187 <nlingareddy@uchicago.edu>
2020-05-14 11:39:51 -0400

    changed

--- templates/script.js
+++ templates/script.js
@@ -15,12 +15,17 @@ function mouseDragged() {
     var synth = new Tone.Synth().toMaster();
     var list = ['A1','A2','A3','A4','A5','A6','A7','B1','B2','B3','B4','B5','B6','B7','C1','C2','C3','C4','C5','C6','C7','D1','D2','D3','D4','D5','D6','D7','E1','E2','E3','E4','E5','E6','E7','F1','F2','F3','F4','F5','F6','F7','G1','G2','G3','G4','G5','G6','G7'];
     //play a middle 'C' for the duration of an 8th note
-    var pick = Math.round(Math.random() * list.length);
+    var pick = Math.round(Math.random() * (list.length - 1));
     var note = list[pick];
     synth.triggerAttackRelease(note, '8n');
     // this draws the same line on everyone else's screen
     // think carefully about the difference between this line:
-    X(`circle(${pmouseX}, ${pmouseY}, ${mouseX} - ${mouseY}); var synth = new Tone.Synth().toMaster(); synth.triggerAttackRelease('${note}', '8n');`)
+    if (Math.random()<0.1){
+      X(`circle(${pmouseX}, ${pmouseY}, ${mouseX} - ${mouseY}); var synth = new Tone.Synth().toMaster(); synth.triggerAttackRelease('${note}', '8n');`)
+    }
+    else{
+       X(`circle(${pmouseX}, ${pmouseY}, ${mouseX} - ${mouseY});`)
+    }
     // and the 'incorrect' X(`line(pmouseX, pmouseY, mouseX, mouseY)`)
     //X(`places.set('${socket.id}', [${pmouseX}, ${pmouseY}])`)
 }
