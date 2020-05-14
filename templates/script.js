b62aedeb54e78444efb6cdc89d782397f72db063
Nicholas Ornstein <44384650+nbso13@users.noreply.github.com>
2020-05-14 10:46:52 -0500

    added in funky circle click n drag

--- templates/script.js
+++ templates/script.js
@@ -1,6 +1,48 @@
+let on; //is there a circle being drawn
+let center_x; //saving center coords
+let center_y;
+let dia; //saving max diam
+let current_dia; //current diam
+let iteration;
+let circle_count;
+let rand_col_1;
+let rand_col_3;
+
+
+
+
 function setup() {
+    on = 2 //neither being drawn nor finishing
+    center_x = 0
+    center_y = 0
+    iteration = 0
+    circle_count = 0
+    rand_col_1 =  Math.floor(Math.random() * 200) + 1
+    rand_col_3 =  Math.floor(Math.random() * 200) + 1
     createCanvas(1024,1024)
     background(128)
+}
+
+
+function draw() {
+  if (on == 0) { //if there is a circle being drawn
+    if (current_dia > dia) { //if the iteration hits the diameter, reset
+      iteration = 1
+    }
+    if (circle_count > 254) {
+      circle_count = 4
+    }
+    current_dia = Math.floor(iteration*dia/100) //calculate current diameter
+    circle(center_x, center_y, current_dia)
+    fill(55+2*iteration, circle_count, Math.floor(Math.random() * 200) + 1)
+    X(`circle(${center_x}, ${center_y}, ${current_dia})`)
+    X('fill(rand_col_1, circle_count, rand_col_3)');
+    iteration = iteration + Math.floor(Math.random() * 10) + 1
+    if (Math.random() < 0.2) {
+      fill(128, 1);
+      rect(0, 0, width, height);
+    }
+  }
 }
 
 function mouseDragged() {
@@ -28,4 +70,18 @@ function mouseDragged() {
     }
     // and the 'incorrect' X(`line(pmouseX, pmouseY, mouseX, mouseY)`)
     //X(`places.set('${socket.id}', [${pmouseX}, ${pmouseY}])`)
+}
+
+function mousePressed() {
+    //otherwise save coordinates and listen for second click
+    center_x = mouseX
+    center_y = mouseY
+    on = 1
+}
+
+function mouseReleased() {
+  //if they already clicked, draw circle using euclidean distance
+  dia = 2*(abs((center_x - mouseX))^2+ abs((center_y-mouseY))^2)^(1/2)
+  on = 0
+  circle_count = circle_count+8;
 }
